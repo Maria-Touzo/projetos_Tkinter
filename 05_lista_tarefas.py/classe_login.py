8f22fce011d725e2d6ccfb0020c06f3955c30053
Maria Julia <maria.touzo@aluno.senai.br>
2025-09-19 11:28:46 -0300

    criando a lista de tarefas

---
+++ 05_lista_tarefas.py/classe_login.py
@@ -0,0 +1,73 @@
+import ttkbootstrap as ttk
+import tkinter.messagebox
+
+class Login:
+
+    def __init__(self):
+        #criando a janela
+        self.janela = ttk.Window(themename= "vapor")
+
+        #título janela
+        self.janela.title("Lista de tarefas ")
+
+        #maximizando a janela
+        self.janela.wm_state("zoomed")
+
+        #não alterar tamanho
+        self.janela.resizable(False, False)
+
+        #escrevendo "login"
+        label_usuario = ttk.Label(self.janela, text= "Login:",
+                                        foreground="white",
+                                        font=("Arial",12 ))
+        label_usuario.pack(pady=10)
+
+        #criando a caixa de texto usuário
+        self.entry_usuario = ttk.Entry(self.janela)
+        self.entry_usuario.pack(pady=10)
+
+        #escrevendo "senha"
+        label_senha = ttk.Label(self.janela, text= "Senha:",
+                                        foreground="white",
+                                        font=("Arial",12 ))
+        label_senha.pack(pady=10)
+
+        #criando a caixa de texto senha
+        self.entry_senha = ttk.Entry()
+        self.entry_senha.pack(pady=10)
+
+        #criando um frame
+        self.frame_botao = ttk.Frame()
+        self.frame_botao.pack()
+
+        #criando o botão login
+        self.botao_login = ttk.Button(self.frame_botao, text= "Logar", padding=10, width=10, command= self.logar )
+        self.botao_login.pack(side="left", padx=10)
+
+        #criando o botão sair
+        sair = ttk.Button(self.frame_botao, text= "Sair", padding=10, width=10  )
+        sair.pack(side="right")
+       
+    def run(self):
+        """Iniciar a janela"""
+        self.janela.mainloop()
+
+    def logar(self):
+        #pegando os valores das caixas de entrada
+        senha_usuario = self.entry_senha.get()
+        login_usuario = self.entry_usuario.get()
+
+        #verificando o login e a senha
+        if login_usuario == "Godofredo" and senha_usuario == "amogirassol":
+            tkinter.messagebox.showinfo(title="Login realizado com sucesso", message="parabéns!")
+        else:
+            tkinter.messagebox.showerror(title="ERRO", message="login ou senha incorreta")
+    
+    def sair(self):
+        exit()
+    
+    
+
+if __name__ == "__main__":
+    janela = Login()
+    janela.run()
